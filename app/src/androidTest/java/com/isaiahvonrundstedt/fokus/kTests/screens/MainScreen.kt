218ae8e15947c39fd934d80a1aada13b4b64a733
Max Borzov <v-barzik@mail.ru>
2024-03-08 14:25:24 +0300

    Форматирование кода

--- app/src/androidTest/java/com/isaiahvonrundstedt/fokus/kTests/screens/MainScreen.kt
+++ app/src/androidTest/java/com/isaiahvonrundstedt/fokus/kTests/screens/MainScreen.kt
@@ -1,36 +1,37 @@
 package com.isaiahvonrundstedt.fokus.kTests.screens
 
 import android.view.View
-import com.kaspersky.kaspresso.screens.KScreen
-import io.github.kakaocup.kakao.toolbar.KToolbar
 import com.isaiahvonrundstedt.fokus.R
 import com.isaiahvonrundstedt.fokus.kTests.FrequencyTestChecks
+import com.kaspersky.kaspresso.screens.KScreen
 import io.github.kakaocup.kakao.check.KCheckBox
 import io.github.kakaocup.kakao.recycler.KRecyclerItem
 import io.github.kakaocup.kakao.recycler.KRecyclerView
 import io.github.kakaocup.kakao.text.KButton
 import io.github.kakaocup.kakao.text.KTextView
+import io.github.kakaocup.kakao.toolbar.KToolbar
 import org.hamcrest.Matcher
 
-object MainScreen: KScreen<MainScreen>(),FrequencyTestChecks{
+object MainScreen : KScreen<MainScreen>(), FrequencyTestChecks {
     override val layoutId: Int? = null
     override val viewClass: Class<*>? = null
 
-    val toolBar = KToolbar {withId(R.id.toolbar)}
-    val toolBarBurgerButton = KButton{
+    val toolBar = KToolbar { withId(R.id.toolbar) }
+    val toolBarBurgerButton = KButton {
         withParent { this@MainScreen.toolBar }
         withContentDescription("More")
     }
-    val toolBarMoreOptionsButton = KButton{
+    val toolBarMoreOptionsButton = KButton {
         withContentDescription("More options")
         withParent { this@MainScreen.toolBar }
     }
-    val toolBarText = KTextView{
+    val toolBarText = KTextView {
         withText("Your Tasks")
         withParent { this@MainScreen.toolBar }
     }
-    val noTaskText = KTextView {withText("No Pending Tasks")}
-    val noTaskSubTitleText = KTextView { withText("You currently have no pending tasks. If you have one, add it using the button below.")}
+    val noTaskText = KTextView { withText("No Pending Tasks") }
+    val noTaskSubTitleText =
+        KTextView { withText("You currently have no pending tasks. If you have one, add it using the button below.") }
     val saveButton = KButton { withId(R.id.actionButton) }
 
     val taskRecycler = KRecyclerView(
@@ -38,10 +39,10 @@ object MainScreen: KScreen<MainScreen>(),FrequencyTestChecks{
         { itemType(MainScreen::TaskItems) }
     )
 
-    class TaskItems(parent: Matcher<View>) :KRecyclerItem<TaskItems>(parent){
-        val testCheckBox = KCheckBox(parent){withId(R.id.checkBox)}
-        val taskName = KTextView(parent){withId(R.id.taskNameView)}
-        val taskEndDate = KTextView(parent){withId(R.id.dueDateView)}
-        val taskSubject = KTextView(parent){withId(R.id.subjectView)}
+    class TaskItems(parent: Matcher<View>) : KRecyclerItem<TaskItems>(parent) {
+        val testCheckBox = KCheckBox(parent) { withId(R.id.checkBox) }
+        val taskName = KTextView(parent) { withId(R.id.taskNameView) }
+        val taskEndDate = KTextView(parent) { withId(R.id.dueDateView) }
+        val taskSubject = KTextView(parent) { withId(R.id.subjectView) }
     }
 }
--- app/src/androidTest/java/com/isaiahvonrundstedt/fokus/kTests/screens/SettingsScreen.kt
+++ app/src/androidTest/java/com/isaiahvonrundstedt/fokus/kTests/screens/SettingsScreen.kt
@@ -1,14 +1,12 @@
 package com.isaiahvonrundstedt.fokus.kTests.screens
 
 import android.view.View
-import androidx.recyclerview.widget.RecyclerView.NO_ID
 import com.isaiahvonrundstedt.fokus.R
 import com.isaiahvonrundstedt.fokus.kTests.FrequencyTestChecks
 import com.kaspersky.kaspresso.screens.KScreen
 import io.github.kakaocup.kakao.image.KImageView
 import io.github.kakaocup.kakao.recycler.KRecyclerItem
 import io.github.kakaocup.kakao.recycler.KRecyclerView
-import io.github.kakaocup.kakao.switch.KSwitch
 import io.github.kakaocup.kakao.text.KButton
 import io.github.kakaocup.kakao.text.KTextView
 import io.github.kakaocup.kakao.toolbar.KToolbar
@@ -29,8 +27,8 @@ object SettingsScreen : KScreen<SettingsScreen>(), FrequencyTestChecks {
         { itemType(::SettingItems) }
     )
 
-    class SettingItems(parent: Matcher<View>) : KRecyclerItem<SettingItems>(parent){
-        val itemTitleText = KTextView(parent) { withId(R.id.title)}
+    class SettingItems(parent: Matcher<View>) : KRecyclerItem<SettingItems>(parent) {
+        val itemTitleText = KTextView(parent) { withId(R.id.title) }
         val itemSubTitle = KTextView(parent) { withId(R.id.summaryView) }
         val itemImage = KImageView(parent) { withId(R.id.icon) }
 
